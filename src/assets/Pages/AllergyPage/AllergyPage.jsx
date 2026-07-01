92f8fc3cbcc63b65a3a4d31e859a1205c89f529e
Deaddrums <jimhorvath@live.nl>
2026-07-01 21:49:59 +0200

    Opdracht start: main lay-out (20%)  home-page (100%)  About page (80%) text unfinished
     allergy / Diet page (30%)

--- src/assets/Pages/AllergyPage/AllergyPage.jsx
+++ src/assets/Pages/AllergyPage/AllergyPage.jsx
@@ -1,11 +1,32 @@
 import './AllergyPage.css'
+import data from '../../Data/AllergyData.json'
+import AllergyInfoCard from "../../Components/AllergyInfoCard/AllergyInfoCard.jsx";
 
 function AllergyPage () {
 
 
     return  <>
 
+<div className="allergyOuterContainer">
 
+    <div className="allergyInnerContainer">
+
+<h1 className="allergyTitle">ALLERGIEËN INFORMATIE</h1>
+
+
+        {data.map((allergy) => (
+            <AllergyInfoCard
+                key={allergy.id}
+                title={allergy.name}
+                description={allergy.description}
+                examples={allergy.products}
+            />
+        ))}
+
+
+    </div>
+
+</div>
 
     </>
 
