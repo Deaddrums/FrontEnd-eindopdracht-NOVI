aef38b6ef4d79d3070061919c84902c08738973f
Deaddrums <jimhorvath@live.nl>
2026-07-19 20:32:44 +0200

    Opdracht start: technical coding login (100%) JWT token storage (100%) save preferences (100%) Private Routes (100%) Allergie Info update Boodschappenlijstgenerator (100%) Bug fixing(100%)

--- src/assets/Pages/RegisterPage/RegisterPage.jsx
+++ src/assets/Pages/RegisterPage/RegisterPage.jsx
@@ -1,12 +1,13 @@
 import './RegisterPage.css'
-import { useState } from "react";
+import {useState} from "react";
 import axios from "axios";
 import image from '../../images/Broodschaap Register.png'
-import { PasswordConfirmer } from "../../Helpers/PasswordConfirmer/PasswordConfirmer.jsx";
-import { ENDPOINTS } from "../../Api/endpoints.js";
+import {PasswordConfirmer} from "../../Helpers/PasswordConfirmer/PasswordConfirmer.jsx";
+import {ENDPOINTS} from "../../Api/endpoints.js";
 import popUpMessage from "../../Components/PopUpMessage/PopUpMessage.jsx";
-import { getFeedbackMessage } from "../../Helpers/GetFeedbackMessage/GetFeedbackMessage.jsx";
+import {getFeedbackMessage} from "../../Helpers/GetFeedbackMessage/GetFeedbackMessage.jsx";
 import PopUpMessage from "../../Components/PopUpMessage/PopUpMessage.jsx";
+import {useNavigate} from "react-router-dom";
 
 function RegisterPage() {
 
@@ -16,6 +17,7 @@ function RegisterPage() {
     const [rpPassword, setRpPassword] = useState('')
     const [rpConfirmPassword, setRpConfirmPassword] = useState('')
     const PROJECT_ID = import.meta.env.VITE_PROJECT_ID
+    const navigate = useNavigate()
 
     const [popup, setPopup] = useState({
         show: false,
@@ -73,6 +75,10 @@ function RegisterPage() {
                 getFeedbackMessage("REGISTER_SUCCESS")
             );
 
+            setTimeout(() => {
+                navigate('/Login');
+            }, 1500);
+
         } catch (error) {
 
             showPopup(
@@ -101,7 +107,7 @@ function RegisterPage() {
         <div className="rpOuterWrapper">
 
             <h1 className="rpTitle"> ACCOUNT REGISTREREN </h1>
-f
+
             {popup.show && (
                 <PopUpMessage
                     message={popup.message}
